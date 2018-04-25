2469c4ad3f0d5a7afee04018feeaf7e301f1cd6b
andrew <andrewsubscribesto+git@gmail.com>
2018-04-24 22:38:03 -0400

    creates a 100x100 image that's defined by a weird ass function

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # hello world
 print("init")
+from PIL import Image
 
-matrix = [[[0 for rgb in range(3)] for y in range(10)] for x in range(10)]
+HEIGHT = 100
+WIDTH = 100
 
-for x in range(10):
-    for y in range(10):
+im = Image.new("RGB",(HEIGHT,WIDTH),color=0)
+pix = im.load()
+
+
+matrix = [[[0 for rgb in range(3)] for y in range(WIDTH)] for x in range(HEIGHT)]
+
+for x in range(HEIGHT):
+    for y in range(WIDTH):
         for rgb in range(3):
-            matrix[x][y][rgb] = x * y * rgb
+            matrix[x][y][rgb] = x * y
+        pix[x,y] = (matrix[x][y][0],matrix[x][y][1],matrix[x][y][2])
+
 print(matrix)
+
+im.save("new.png")
