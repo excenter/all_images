6a119d76d98f5a57fda7e118ca9d96f822c5a145
andrew <andrewsubscribesto+git@gmail.com>
2018-04-24 22:05:51 -0400

    basic 3 dimensional array

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # hello world
 print("init")
+
+matrix = [[[0 for rgb in range(3)] for y in range(10)] for x in range(10)]
+
+for x in range(10):
+    for y in range(10):
+        for rgb in range(3):
+            matrix[x][y][rgb] = x * y * rgb
+print(matrix)
